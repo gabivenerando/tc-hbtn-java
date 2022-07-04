6b52b8d4df919b67bdff8b1de8131763da783a57
Gabrielli Venerando <107216833+gabivenerando@users.noreply.github.com>
2022-07-04 13:35:23 -0300

    Update Celular.java

--- Enum/celular/Celular.java
+++ Enum/celular/Celular.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Celular {
-    private ArrayList<Contato> contato = new ArrayList<>();
+    ArrayList<Contato> contato = new ArrayList<>();
 
 
     public int obterPosicaoContato(String nome){
