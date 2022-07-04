5045282d5a12098cce87cc6bb0f29f5fe5e236a4
Gabrielli Venerando <107216833+gabivenerando@users.noreply.github.com>
2022-07-04 00:34:40 -0300

    task 0

---
+++ Enum/celular/Celular.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class Celular {
+    private ArrayList<Contato> contato = new ArrayList<>();
+
+
+    public int obterPosicaoContato(String nome){
+
+        int verificacao = 0;
+
+        if(contato.size() == 0){
+            return  verificacao = -1;
+        }
+
+
+
+        for(Contato nomes : contato){
+            if(nomes.getNome().equals(nome)){
+
+               return verificacao = nomes.getNome().indexOf(nome) ;
+            }else{
+                verificacao = -1;
+            }
+        }
+
+
+
+        return verificacao;
+    }
+
+    public void adicionarContato(Contato contato) throws Exception{
+
+        int verificacao = obterPosicaoContato(contato.getNome());
+
+        if(verificacao == -1 || verificacao == 0){
+            this.contato.add(contato);
+        }else{
+            throw new Exception("Nao foi possivel adicionar contato. Contato jah existente com esse nome");
+        }
+    }
+
+    public void atualizarContato(Contato contatoAntigo, Contato novoContato) throws Exception {
+        int verificacaoContatoAntigo = obterPosicaoContato(contatoAntigo.getNome());
+
+        if(verificacaoContatoAntigo != -1){
+
+        }else{
+            throw new Exception("Nao foi possivel modificar contato. Contato nao existe");
+        }
+
+        int verificacaoNovoContato = obterPosicaoContato(novoContato.getNome());
+
+
+        if(verificacaoContatoAntigo != -1 && verificacaoNovoContato != -1 && contatoAntigo.getNumero() != novoContato.getNumero() && contatoAntigo.getNome() == novoContato.getNome()) {
+               this.contato.remove(contatoAntigo);
+               this.contato.add(novoContato);
+
+        }else{
+            throw new Exception("Nao foi possivel modificar contato. Contato jah existente com esse nome");
+        }
+    }
+
+    public void removerContato(Contato contato) throws Exception {
+            int verficacao = obterPosicaoContato(contato.getNome());
+
+            if(verficacao != -1){
+                this.contato.remove(contato);
+            }else{
+                throw new Exception("Nao foi possivel remover contato. Contato nao existe");
+            }
+    }
+
+    public void listarContatos(){
+
+        Collections.sort(contato);
+
+        for(Contato nomes : contato){
+            System.out.println(nomes);
+        }
+
+    }
+}
+
+
---
+++ Enum/celular/Contato.java
@@ -0,0 +1,48 @@
+public class Contato implements Comparable<Contato>{
+    private String nome;
+    private String numero;
+    private TipoNumero tiponumero;
+
+    public Contato(String nome, String numero, TipoNumero tiponumero) {
+        this.nome = nome;
+        this.numero = numero;
+        this.tiponumero = tiponumero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public TipoNumero getTiponumero() {
+        return tiponumero;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (!(obj instanceof Contato)) {
+            return false;
+        }
+        Contato contato = (Contato) obj;
+        return (this.nome == contato.getNome());
+
+    }
+
+    @Override
+    public String toString() {
+        return this.getNome() + " -> " + this.numero + " (" + this.tiponumero + ")";
+    }
+
+    @Override
+    public int compareTo(Contato outroContato) {
+        Contato atual = (Contato) outroContato;
+        return this.nome.compareTo(atual.getNome());
+    }
+}
