8ad961a1ac541a30b38a7447aecc31264c7cd14d
Gabrielli Venerando <107216833+gabivenerando@users.noreply.github.com>
2022-07-07 14:18:08 -0300

    Add files via upload

---
+++ Generics_Exercises/armazem/Armazem.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Armazem <T> implements Armazenavel <T>{
+    private Map<String, T> mapList = new HashMap<>();
+
+    @Override
+    public void adicionarAoInventario(String nome, T valor) {
+       this.mapList.put(nome, valor);
+    }
+
+    @Override
+    public T obterDoInventario(String nome) {
+
+        T result = null;
+
+            if(mapList.containsKey(nome)){
+               result = mapList.get(nome);
+            }
+
+        return result;
+    }
+}
