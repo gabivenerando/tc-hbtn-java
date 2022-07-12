b05c1e5d42e068c5151e83bceb5facfa623f72cb
Gabrielli Venerando <107216833+gabivenerando@users.noreply.github.com>
2022-07-12 12:19:55 -0300

    Add files via upload

---
+++ Streams_Collectors_Optional/preco_minimo/Consulta.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Comparator.comparing;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido){
+
+        List<Produto> listProdutos = pedido.getProdutos();
+        List<Produto> newList = listProdutos.stream().filter(produto -> produto.getCategoriaProduto().equals(CategoriaProduto.LIVRO)).collect(Collectors.toList());
+
+        return newList;
+    };
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos){
+        List<Produto> ordemReversa = produtos.stream().sorted(comparing(Produto::getPreco).reversed()).limit(1).collect(Collectors.toList());
+
+        return ordemReversa.get(0);
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo){
+        List<Produto> produtosPrecoMinimo = produtos.stream().filter( produto -> produto.getPreco() >= precoMinimo).collect(Collectors.toList());{
+
+        }
+
+        return produtosPrecoMinimo;
+    }
+}
